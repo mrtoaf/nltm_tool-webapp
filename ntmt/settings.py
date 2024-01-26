e3d63add6844bf940aedc0faf2245d0d262ba543
Rz2750 <rz12750@gmail.com>
2024-01-26 14:14:37 -0500

    d3 exploration
    Used /pytm_web url to test out D3 visualization

--- ntmt/settings.py
+++ ntmt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1)y8msn7$=4c#8924c@pj1g+1_g(s&y&bo1=9l#v6h))%p@=ug
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
